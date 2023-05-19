cf7fe4b4b92898425c1bc09b1f0a37963e2f8a67
MacErikson <133850916+MacErikson@users.noreply.github.com>
2023-05-19 18:57:48 +0200

    Update main.py
    Added background

--- main.py
+++ main.py
@@ -6,12 +6,28 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Fruit Invaders')
 
+
+#load in image
+bg = pygame.image.load("Projekt/FruitInvaders/img/bg.png")
+
+def draw_bg():
+    screen.blit(bg, (0,0))
+
 run = True
 while run:
+
+
+
+    #draw background
+    draw_bg()
+
+
 
     #event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run == False
 
-pygame.quit()
+    pygame.display.update()
+
+pygame.QUIT()
