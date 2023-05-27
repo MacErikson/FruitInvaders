2d2bf1aa1f8eb23da184d4ae623fdde0b652fc4e
MacErikson <ErikRedlin@web.de>
2023-05-27 13:29:14 +0200

    renaming and adding sprites

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #define fps
 clock = pygame.time.Clock()
@@ -9,6 +10,10 @@ screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Fruit Invaders')
+
+#variablen
+rows = 5
+cols = 5
 
 #define colors
 red = (255, 0, 0)
@@ -30,9 +35,11 @@ class spaceship(pygame.sprite.Sprite):
         self.rect.center = [x, y]
         self.health_start = health
         self.health_remaining = health
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         speed = 8
+        cooldown = 500
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.left > 0:
@@ -43,10 +50,14 @@ class spaceship(pygame.sprite.Sprite):
             self.rect.y -= speed
         if key[pygame.K_DOWN] and self.rect.bottom < screen_height -20:
             self.rect.y += speed
+
+        #Zeit
+        time_now = pygame.time.get_ticks()
         #shoot
-        if key[pygame.K_SPACE]:
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
             bullet = Bullets(self.rect.centerx, self.rect.top)
             bullet_group.add(bullet)
+            self.last_shot = time_now
         
         #draw healthbar
         pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
@@ -63,10 +74,35 @@ class Bullets(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()
+
+#Food
+class Food(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/food" + str(random.randint(1, 80)) + ".png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        pass
+
+
         
 #Groups
 spaceship_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
+food_group = pygame.sprite.Group()
+
+def create_food():
+    for row in range(rows):
+        for item in range(cols):
+            food = Food(100 +item * 100, 100 + row * 70)
+            food_group.add(food)
+
+create_food()
+
 
 #Player
 spaceship = spaceship(int(screen_width / 2), screen_height - 100, 3)
@@ -92,6 +128,7 @@ while run:
     spaceship.update()
     bullet_group.update()
     pygame.display.update()
+    food_group.update()
 
     #draw backgroundw
     draw_bg()
@@ -99,5 +136,6 @@ while run:
     #draw sprite groups
     spaceship_group.draw(screen)
     bullet_group.draw(screen)
+    food_group.draw(screen)
 
 pygame.QUIT()
